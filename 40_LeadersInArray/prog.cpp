ac977596f6cc054c3686879e4f3df234a49d9bbe
Shakun BEETAN <shakun.beetan@amadeus.com>
2021-01-23 21:31:41 +0530

    Find leaders in an array

---
+++ 40_LeadersInArray/prog.cpp
@@ -0,0 +1,19 @@
+// leader is an element which is greater than all elements right to it.
+vector<int> leaders(int a[], int n){
+    // code here
+    vector<int> leaders;
+    int max_so_far = a[n-1];
+    leaders.push_back(max_so_far);
+    int j=1;
+    for(int i=n-2; i>=0; i--)
+    {
+        if(max_so_far <= a[i])
+        {
+            max_so_far = a[i];
+            leaders.push_back(a[i]);
+        }
+        
+    }
+    reverse(leaders.begin(), leaders.end());
+    return leaders;
+}
